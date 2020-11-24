b65cb6085691a6fa57480b435a8c6cec21ad3562
mariamqader17 <49173590+mariamqader17@users.noreply.github.com>
2020-11-23 19:33:35 -0800

    Add files via upload

--- run.py
+++ run.py
@@ -1,3 +1,29 @@
-#code to run
-def main(info):
-  return info
+import sys
+import os
+import json
+sys.path.insert(0, 'src')
+
+from all import findFeatures
+
+def main(targets):
+    '''
+    Runs the main project pipeline logic, given the targets.
+    targets must contain: 'data', 'analysis', 'model'. 
+    
+    `main` runs the targets in order of data=>analysis=>model.
+    '''
+
+    if 'test' in targets:
+        with open('config/test_params.json') as fh:
+            feature_cfg = json.load(fh)
+
+        # make the data target
+        features, labels = findFeatures(**feature_cfg)
+
+    return 
+
+if __name__ == '__main__':
+    # run via:
+    # python main.py data features model
+    targets = sys.argv[1:]
+    main(targets)
